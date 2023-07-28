f34973d094c5c22467209bfc563f3a0de07ca21e
shindeprem <prembshinde1210@gmail.com>
2023-07-28 15:22:36 +0530

    --fix changes in dropdown css

--- StyleSquare/src/pages/Main/mainPage.jsx
+++ StyleSquare/src/pages/Main/mainPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/header";
+import CarouselHeader from "../../components/main-page-carousel/carouselHeader";
 import './mainPage.css'
 
 const MainPage =()=>{
     return(
     <>
     <Header/>
+    <CarouselHeader/>
     </>
     )
 }
