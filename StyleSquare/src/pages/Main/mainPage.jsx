7145b5b9ab62cd5c35930d2ed46a967e4c348a2b
shindeprem <prembshinde1210@gmail.com>
2023-08-01 15:37:38 +0530

    adding components to mainPage

--- StyleSquare/src/pages/Main/mainPage.jsx
+++ StyleSquare/src/pages/Main/mainPage.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "../../components/Header/header";
 import CarouselHeader from "../../components/main-page-carousel/carouselHeader";
+import ProductsCarousel from "../../components/FetchApiProducts/productsCarousel";
 import './mainPage.css'
 
 const MainPage =()=>{
+
+
     return(
     <>
     <Header/>
     <CarouselHeader/>
+    {/* Add Product list of category shirts */}
+    <ProductsCarousel category='shirt'/>
+    {/* Add Product list of category clothes */}
+    <ProductsCarousel category='saree'/>
+    {/* Add Infinite moving lists */}
+    {/* Add another infinite moving list */}
+    {/* Add another product lists */}
+    {/* Add another product lists */}
+    {/* Footer */}
     </>
     )
 }
