15766fcd432165178a17eceacffdb3072d232258
shindeprem <prembshinde1210@gmail.com>
2023-08-06 18:08:42 +0530

    Added media queries for tab screen

--- StyleSquare/src/components/Footer/footer.jsx
+++ StyleSquare/src/components/Footer/footer.jsx
@@ -6,48 +6,52 @@ import './footer.css'
 const Footer = ()=>{
     return(
         <div className="footer-section">
-            <div className="to-pages">
-                <h2>Checkout</h2>
-                <div className="about-page">
-                    About
-                </div>
-                <div className="contact-page">
-                    Contact Us
-                </div>
-                <div className="customer-page">
-                    Customer
-                </div>
-                <div className="deals-page">
-                    Deals
-                </div>
-            </div>
 
-            <div className="category-sections">
-                <h2>Categories</h2>
-                <div className="mens-section">Mens</div>
-                <div className="womens-section">Womens</div>
-                <div className="kids-section">Kids</div>
-                <div className="mens-section">Accessories</div>
-            </div>
-
-            <div className="all-socials">
-                <div className="facebook-container">
-                    <AiFillFacebook/>
+            <div className="footer-div">
+                <div className="to-pages">
+                    <h2>Checkout</h2>
+                    <div className="about-page">
+                        About
+                    </div>
+                    <div className="contact-page">
+                        Contact Us
+                    </div>
+                    <div className="customer-page">
+                        Customer
+                    </div>
+                    <div className="deals-page">
+                        Deals
+                    </div>
                 </div>
-                <div className="instagram-container">
-                    <BsInstagram/>
-                </div>
-            </div>
 
-            <div className="subscribe-for-deals">
-                <form>
-                    <input type="email" name="user-email" id="" placeholder="Plaese Enter your email"/>
-                    <button type="submit" className="subscribe-btn">
-                        Subscribe
-                    </button>
-                </form>
+                <div className="category-sections">
+                    <h2>Categories</h2>
+                    <div className="mens-section">Mens</div>
+                    <div className="womens-section">Womens</div>
+                    <div className="kids-section">Kids</div>
+                    <div className="mens-section">Accessories</div>
+                </div>
+
+                <div className="all-socials">
+                    <div className="facebook-container">
+                        <AiFillFacebook/>
+                    </div>
+                    <div className="instagram-container">
+                        <BsInstagram/>
+                    </div>
+                </div>
+
+                <div className="subscribe-for-deals">
+                    <form>
+                        <input type="email" name="user-email" id="" placeholder="Plaese Enter your email"/>
+                        <button type="submit" className="subscribe-btn">
+                            Subscribe
+                        </button>
+                    </form>
                 
+                </div>
             </div>
+            
         </div>
     )
 }
