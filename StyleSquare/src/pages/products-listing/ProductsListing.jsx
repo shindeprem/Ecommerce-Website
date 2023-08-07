6059bb977eb88d707a8aa0d0ba8482b88b59ea4d
shindeprem <prembshinde1210@gmail.com>
2023-08-07 23:21:23 +0530

    Added products listing

--- StyleSquare/src/pages/products-listing/ProductsListing.jsx
+++ StyleSquare/src/pages/products-listing/ProductsListing.jsx
@@ -2,10 +2,10 @@ import './ProductsListing.css'
 
 const ProductListing = ()=>{
     return(
-        <>
+        <section className='product-listing-page'>
         <h1>Hello</h1>
         <h1>This is product listing page</h1>
-        </>
+        </section>
     )
 }
 export default ProductListing;
