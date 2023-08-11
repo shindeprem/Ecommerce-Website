08cb15c322045c6d7e1f481a4539ce9733ff177b
shindeprem <prembshinde1210@gmail.com>
2023-08-11 20:55:05 +0530

    Added product layout

--- StyleSquare/src/pages/Products-Layout/ProductsLayout.jsx
+++ StyleSquare/src/pages/Products-Layout/ProductsLayout.jsx
@@ -2,13 +2,12 @@ import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer/footer";
 import Header from "../../components/Header/header";
 
-
 const ProductsLayout = ()=>{
     return(
         <>
-            <Header/>
+            <Header className="header-section"/>
             <Outlet/>
-            <Footer/>
+            <Footer className="footer-section"/>
         </>
     )
 }
