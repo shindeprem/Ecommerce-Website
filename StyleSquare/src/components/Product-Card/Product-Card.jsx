a30743d46dd9e1eea4a3233aff3a32c2aef86aaa
shindeprem <prembshinde1210@gmail.com>
2023-08-11 20:55:50 +0530

    Adding product card

---
+++ StyleSquare/src/components/Product-Card/Product-Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Product-Card.css'
+
+const ProductCard = ({product})=>{
+    return(
+        <div className="product-card">
+
+        </div>
+    )
+}
+
+export default ProductCard;
