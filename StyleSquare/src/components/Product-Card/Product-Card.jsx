42916ef09058968cf35612a2670410531860f860
shindeprem <prembshinde1210@gmail.com>
2023-08-25 18:47:50 +0530

    --feat added product card

--- StyleSquare/src/components/Product-Card/Product-Card.jsx
+++ StyleSquare/src/components/Product-Card/Product-Card.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import './Product-Card.css'
+import {BsBag} from 'react-icons/bs'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 
-const ProductCard = ({product})=>{
+const ProductCard = ()=>{
     return(
         <div className="product-card">
+            <div className="img-container">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrQGflCsSDWOf7lxO0rEKy9HNZrfpNPzZtSg&usqp=CAU" alt="" className="product-img" />
 
+                <div className="discount-card">
+                    <p className="discount-amt">
+                        15% Off
+                    </p>
+                </div>
+            </div>
+
+            <div className="product-details">
+                <div className="product-name-heading">
+                    <h2 className="product-name">
+                        Black autty Shirt
+                    </h2>
+                </div>
+                <div className="product-description">
+                    <p className="product-desc">
+                        Lorem ipsum dolor sit amet, consec tetur adipisicing elit...
+                    </p>
+                </div>
+                <div className="product-price">50$</div>
+
+                <div className="btn-container">
+                    <button className="add-to-cart"><AiOutlineShoppingCart className="cart-icon"/>Add to cart</button>
+                    <button className="buy-now-btn"><BsBag className="bag-icon"/> Buy Now</button>
+                </div>
+            </div>
+            
         </div>
     )
 }
---
+++ StyleSquare/src/pages/products-listing/ProductsListing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductCard from '../../components/Product-Card/Product-Card'
+
+const ProductListing = ()=>{
+    return(
+        <div className="product-listing">
+            <ProductCard/>
+        </div>
+    )
+}
+
+export default ProductListing;
