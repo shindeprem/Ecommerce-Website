1e382a438f69332d70b8641b2e598a27434e309c
shindeprem <prembshinde1210@gmail.com>
2023-08-29 13:01:49 +0530

    --feat Added colors filters

--- StyleSquare/src/components/Filters/filters.jsx
+++ StyleSquare/src/components/Filters/filters.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './filter.css'
 import { useContext } from "react";
 
 
 const Filters = () =>{
 
-    const selectColors = ['pink','red','yellow','gray','black','orange']
+    const selectColors = ['red','pink','yellow','gray','black','orange']
     const [selectedColors,setSelectedColors] = useState([])
+    const addColor = (color) => {
+        if (selectedColors.includes(color)) {
+            setSelectedColors((prevSelectedColors) => prevSelectedColors.filter((selectedColor) => selectedColor !== color));
+        } else {
+            setSelectedColors([...selectedColors, color]);
+        }
+    }
+    
+
+    useEffect(()=>{
+            console.log(selectedColors);
+    },[selectedColors])
+    
+
     
     return(
         // filter, sorting, pagination
@@ -16,10 +30,17 @@ const Filters = () =>{
                     <div className="colors-section">
                         <h2 className="colors-filter-heading">Filter by colors</h2>
                         <div className="colors-selection-filters">
-                            {selectColors.map((color)=>{
+                            {selectColors.map((color,index)=>{
                                 return(
-                                    <div className="color-container" style={{backgroundColor:
-                                        color}}><h3>{color.slice(0,1).toLocaleUpperCase()}</h3></div>
+                                    <div 
+                                    style={{backgroundColor:
+                                        color,}} 
+                                    onClick={()=>addColor(color)} 
+                                    key={index}
+                                    className={`${selectedColors.includes(color)?'selected':''} color-container`}
+                                    >
+                                        <h3>{color.slice(0,1).toLocaleUpperCase()}</h3>
+                                    </div>
                                 )
                             })}
                         </div>
