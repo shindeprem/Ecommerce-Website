1eb7f53b5c99e77c2800705a64f8855dabe791f3
shindeprem <prembshinde1210@gmail.com>
2023-09-03 16:40:45 +0530

    Initial changes in Product page

--- StyleSquare/src/components/Product-Page/ProductPage.jsx
+++ StyleSquare/src/components/Product-Page/ProductPage.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import './ProductPage.css'
 
 const ProductPage = ()=>{
+    const {id}=useParams()
+    // console.log(id);
     return(
-        <></>
+        <div className="product-page-container">
+            <div className="product-img-carousel">
+                
+            </div>
+            <div className="product-main-photo-conntainer">
+
+            </div>
+            <div className="product-info-container">
+
+            </div>
+        </div>
     )
 }
 
