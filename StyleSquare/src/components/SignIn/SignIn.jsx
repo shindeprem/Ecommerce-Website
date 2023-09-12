16c4c3dbcaf3e538bf436212acf21c07d70956b2
shindeprem <prembshinde1210@gmail.com>
2023-09-12 21:33:44 +0530

    Updated signin css

--- StyleSquare/src/components/SignIn/SignIn.jsx
+++ StyleSquare/src/components/SignIn/SignIn.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import {AiOutlineUser} from 'react-icons/ai';
 import {BsShieldLock} from 'react-icons/bs'
 import {BsApple} from 'react-icons/bs'
@@ -6,39 +5,39 @@ import {FcGoogle} from 'react-icons/fc'
 import img from '../../assets/images/signin-signup-img.png'
 import './SignIn.css'
 
+
 const SignIn = ()=>{
     return(
-
-        <div className="signup-page">
-            <div className="signup-page-container">
-                <div className="signup-img-container">
+        <div className="signin-page">
+            <div className="signin-page-container">
+                <div className="signin-img-container">
                     <img src={img} alt="ecommerce-image" />
                 </div>
-                <div className="signup-inputs-container">
-                <div className="input-header-container">
-                        SIGN In
-                </div>
-                <div className="signup-email-container">
-                <div className="signup-email-input">
-                    <AiOutlineUser className="user-icon"/>
-                    <input type="email" className="email-input-container" placeholder="Enter email address..."
-                                    />
-                </div>
-                                
-               </div>
-                <div className="signup-password-input-container">
-                    <div className="signup-password-input">
-                        <BsShieldLock className="lock-icon"/>
-                        <input type="password" className="passwor-input" placeholder="Enter Password"
-                                    />
+                <div className="signin-inputs-container">
+                    <div className="input-header-container">
+                        SIGN IN
                     </div>
-                </div>
-                <div className="signup-btn-container">
-                        <button className="signup-btn">SignUp
+                    <div className="signin-email-container">
+                        <div className="signin-email-input">
+                            <AiOutlineUser className="user-icon"/>
+                            <input type="email" className="email-input-container" placeholder="Enter email address..."
+                            />
+                        </div>
+                        
+                    </div>
+                    <div className="signin-password-input-container">
+                        <div className="signin-password-input">
+                            <BsShieldLock className="lock-icon"/>
+                            <input type="password" className="passwor-input" placeholder="Enter Password"
+                            />
+                        </div>
+                    </div>
+                    <div className="signin-btn-container">
+                        <button className="signin-btn">SignIn
                         </button>
-                </div>
-                <div className="bottom-border"/>
-        
+                    </div>
+                    <div className="bottom-border"/>
+
                     <div className="social-media-container">
                         <div className="google-icon-container">
                             <FcGoogle className="socials-icon google-icon"/>
