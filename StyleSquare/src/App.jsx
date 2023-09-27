f38534f622418bdafad079bf284bc1f18bda2a9f
shindeprem <prembshinde1210@gmail.com>
2023-09-27 21:00:07 +0530

    --feat Added reducer function

--- StyleSquare/src/App.jsx
+++ StyleSquare/src/App.jsx
@@ -9,13 +9,67 @@ import Filters from "./components/Filters/filters";
 import ProductPage from "./components/Product-Page/ProductPage";
 import SignIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp/SignUp";
+import UserCart from "./components/UserCart/UserCart";
+
+export const CartContext = createContext();
+
+const products = []
+
+const initialState = {
+    item:products,
+    totalAmount:25600,
+    totalItems:0,
+    quantity:1,
+}
+
 
 const App =()=>{
 
     // Add context here
 
+
+    const [state,dispatch] = useReducer(reducer,initialState)
+
+    const clearCart = () => {
+        return dispatch({ type: "clear-cart" });
+    };
+
+    const removeItem = (id) => {
+        return dispatch({
+            type: "Remove-Item",
+            payload: id,
+        });
+    };
+
+  const increment = (id) => {
+    return dispatch({
+      type: "increment",
+      payload: id,
+    });
+  };
+
+  const decrement = (id) => {
+    return dispatch({
+      type: "decrement",
+      payload: id,
+    });
+  };
+
+  const addItem = (id,name) =>{
+    return dispatch({
+        type:'add-to-cart',
+        payload:{
+            id:id,
+            name:name,
+        }
+    })
+  }
+
     return(
         <>
+        <CartContext.Provider value={{...state,addItem,clearCart,increment,decrement,removeItem}}>
+            <UserCart/>
+        </CartContext.Provider>
         <Routes>
             <Route path="/" element={<ProductsLayout/>}>
                 <Route index element={<MainPage/>}/>
---
+++ StyleSquare/src/components/UserCart/UserCart.jsx
@@ -0,0 +1,7 @@
+const UserCart = ()=>{
+    return(
+        <></>
+    )
+}
+
+export default UserCart;
