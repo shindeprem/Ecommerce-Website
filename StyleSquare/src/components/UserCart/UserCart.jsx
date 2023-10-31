00ec77bf90403cc1b9f015904e7dd3ed31823631
shindeprem <prembshinde1210@gmail.com>
2023-10-31 11:46:11 +0530

    imported css file

--- StyleSquare/src/components/UserCart/UserCart.jsx
+++ StyleSquare/src/components/UserCart/UserCart.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import CartProduct from "./CartProduct";
 import { CartContext } from "../../App";
+import './UserCart.css'
 import img1 from '../../assets/images/blue-striped-shirt.jpg'
 
 const UserCart = ()=>{
