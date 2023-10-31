f0816472f4746371fd46ef08a9da30eb3da1582b
shindeprem <prembshinde1210@gmail.com>
2023-10-31 13:23:35 +0530

    feat: Added cart item container

--- StyleSquare/src/components/UserCart/UserCart.jsx
+++ StyleSquare/src/components/UserCart/UserCart.jsx
@@ -3,6 +3,8 @@ import CartProduct from "./CartProduct";
 import { CartContext } from "../../App";
 import './UserCart.css'
 import img1 from '../../assets/images/blue-striped-shirt.jpg'
+import {FiPlus} from 'react-icons/fi'
+import {AiOutlineMinus} from 'react-icons/ai'
 
 const UserCart = ()=>{
 
@@ -11,37 +13,37 @@ const UserCart = ()=>{
     const item = [
         {
             name:'Shirt hai bhai',
-            price:'$190',
+            price:190,
             image:img1,
             quanitity:4,
         },
         {
             name:'Shirt hai bhai',
-            price:'$190',
+            price:190,
             image:img1,
             quanitity:4,
         },
         {
             name:'Shirt hai bhai',
-            price:'$190',
+            price:190,
             image:img1,
             quanitity:4,
         },
         {
             name:'Shirt hai bhai',
-            price:'$190',
+            price:190,
             image:img1,
             quanitity:4,
         },
         {
             name:'Shirt hai bhai',
-            price:'$190',
+            price:190,
             image:img1,
             quanitity:4,
         },
         {
             name:'Shirt hai bhai',
-            price:'$190',
+            price:190,
             image:img1,
             quanitity:4,
         },
@@ -50,7 +52,39 @@ const UserCart = ()=>{
         <div className="cart-items-container">
             {item.map((item)=>{
                 return(
-                    <>{item.name}</>
+                    <div className="cart-item">
+                        <div className="cart-item-img">
+                            <img src={item.image}/>
+                        </div>
+                        <div className="cart-item-info">
+                            <div className="cart-item-heading">
+                                {item.name}
+                            </div>
+
+                            <div className="total-items-price">
+                                <div className="items-price-heading">
+                                    Total Price :
+                                </div>
+                                <div className="items-price">
+                                    ${item.price*item.quanitity}
+                                </div>
+                                
+                            </div>
+
+                            <div className="quantity-incr-decr-btn">
+                                <div className="incr-btn-container">
+                                    <FiPlus/>
+                                </div>
+                                <input type="number" placeholder={item.quanitity} disabled/>
+                                <div className="decr-btn-container">
+                                    <AiOutlineMinus/>
+                                </div>
+                            </div>
+
+                            <button className="remove-item-btn">Remove Item</button>
+                        </div>
+                        
+                    </div>
                 )
             })}
         </div>
