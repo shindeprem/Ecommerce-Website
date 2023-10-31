206ebae38569d65f61f95f7dcebe1678632f614e
shindeprem <prembshinde1210@gmail.com>
2023-10-31 11:45:26 +0530

    Adding css files

--- StyleSquare/src/components/UserCart/UserCart.jsx
+++ StyleSquare/src/components/UserCart/UserCart.jsx
@@ -1,11 +1,50 @@
 import { useContext } from "react";
 import CartProduct from "./CartProduct";
 import { CartContext } from "../../App";
+import img1 from '../../assets/images/blue-striped-shirt.jpg'
 
 const UserCart = ()=>{
 
-    const {item,increment,decrement,clearCart,totalItems} = useContext(CartContext)
+    // const {item,increment,decrement,clearCart,totalItems} = useContext(CartContext)
 
+    const item = [
+        {
+            name:'Shirt hai bhai',
+            price:'$190',
+            image:img1,
+            quanitity:4,
+        },
+        {
+            name:'Shirt hai bhai',
+            price:'$190',
+            image:img1,
+            quanitity:4,
+        },
+        {
+            name:'Shirt hai bhai',
+            price:'$190',
+            image:img1,
+            quanitity:4,
+        },
+        {
+            name:'Shirt hai bhai',
+            price:'$190',
+            image:img1,
+            quanitity:4,
+        },
+        {
+            name:'Shirt hai bhai',
+            price:'$190',
+            image:img1,
+            quanitity:4,
+        },
+        {
+            name:'Shirt hai bhai',
+            price:'$190',
+            image:img1,
+            quanitity:4,
+        },
+    ]
     return(
         <div className="cart-items-container">
             {item.map((item)=>{
